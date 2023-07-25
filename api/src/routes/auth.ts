5f6fc1b700214459747aeb32adbe4fdf44a3190d
wiktor-falek <falekwiktor@gmail.com>
2023-07-25 23:35:54 +0200

    add /auth route that lets client verify if authorized

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register, login } from "../controllers/user.js";
+import { register, login, isAuthorized } from "../controllers/auth.js";
 
 const router = Router();
 
-router.post("/auth/register/:accessCode", register);
+router.post("/auth/register", register);
 router.post("/auth/login", login);
+router.get("/auth", isAuthorized);
 
 export default router;
