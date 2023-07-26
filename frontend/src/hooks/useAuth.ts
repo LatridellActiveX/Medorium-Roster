c85ae51181dc378a2c1fc9e0aac656916d4b68c3
wiktor-falek <falekwiktor@gmail.com>
2023-07-26 16:00:49 +0200

    fix cookie not being sent correctly

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -4,16 +4,18 @@ import { authorizeUser } from "../redux/reducers/authReducer";
 import { useDispatch } from "react-redux";
 
 const useAuth = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        axios.get('http://localhost:3000/auth').then(resp => {
-            if (resp.status === 200) {
-                console.log(resp)
-                dispatch(authorizeUser(resp.data.username));
-            };
-        })
-    }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auth", { withCredentials: true })
+      .then((resp) => {
+        if (resp.status === 200) {
+          console.log(resp);
+          dispatch(authorizeUser(resp.data.username));
+        }
+      });
+  }, []);
 };
 
 export default useAuth;
