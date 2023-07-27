badf5fe44b567ae4d0ef8efff6a291876cb6b373
Xlowen <www.lowenml@gmail.com>
2023-07-27 11:51:10 -0300

    create middleware authenticated

--- api/src/middlewares/authorize.ts
+++ api/src/middlewares/authorize.ts
@@ -1,13 +1,17 @@
-import type { Request, Response, NextFunction } from "express";
+import type { NextFunction, Request, Response } from "express";
 import User from "../models/user.js";
 import { decodeAuthToken } from "../utils/authToken.js";
+import { z } from "zod"
 
-export async function isAuthorized(req: Request, res: Response, next: NextFunction) {
-    const { authToken } = req.cookies;
-  
-    if (typeof authToken !== "string") {
+async function authorized(req: Request, res: Response, next: NextFunction) {
+
+    const authTokenSchema = z.string().min(100).max(200);
+    const validation = authTokenSchema.safeParse(req.cookies.authToken);
+
+    if (!validation.success) {
       return res.status(401).json({ error: "Not Authorized" });
     }
+    const authToken = validation.data;
   
     const payload = decodeAuthToken(authToken);
   
@@ -24,5 +28,6 @@ export async function isAuthorized(req: Request, res: Response, next: NextFuncti
     }
   
     next();
-    
   }
+
+ export default authorized;
