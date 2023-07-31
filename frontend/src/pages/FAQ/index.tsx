a36ee0391d790fab53f1075445abc0e08df2c883
podpison <au25557@yandex.ru>
2023-07-31 11:47:03 +0500

    privacy policy page

--- frontend/src/pages/FAQ/index.tsx
+++ frontend/src/pages/FAQ/index.tsx
@@ -5,43 +5,44 @@ import { ParagraphType } from "../../ui/paragraphs/paragraph";
 const data: ParagraphType[] = [
     {
         heading: 'What is EVE Online?',
-        text: 'EVE Online is a massively multiplayer online game set in space, where players can explore, engage in combat, and build their own empires.'
+        texts: 'EVE Online is a massively multiplayer online game set in space, where players can explore, engage in combat, and build their own empires.'
     },
     {
         heading: 'What is the Medor Roster website?',
-        text: 'The Medor Roster website is a platform that allows you to view your character information from EVE Online in a table format. //Not really, they would think this is connected to eve\'s api, it is not'
+        texts: 'The Medor Roster website is a platform that allows you to view your character information from EVE Online in a table format. //Not really, they would think this is connected to eve\'s api, it is not'
     },
     {
         heading: 'How do I access the Medor Roster website?',
-        text: 'To access the Medor Roster website, you need to have a registration link provided by your admin. Once you have the link, simply navigate to the website though the link and enter your registration credentials to access the website and your information.'
+        texts: 'To access the Medor Roster website, you need to have a registration link provided by your admin. Once you have the link, simply navigate to the website though the link and enter your registration credentials to access the website and your information.'
     },
     {
         heading: 'What kind of information can I view on the Medor Roster website?',
-        text: 'On the Medor Roster website, you can view information about your EVE Online character, such as their name, race, and skills.'
+        texts: 'On the Medor Roster website, you can view information about your EVE Online character, such as their name, race, and skills.'
     },
     {
         heading: 'Can I register for the Medor Roster website without a registration code?',
-        text: 'No, you cannot register for the Medor Roster website without a registration code provided by your admin.',
+        texts: 'No, you cannot register for the Medor Roster website without a registration code provided by your admin.',
         id: 'registrationCode'
     },
     {
         heading: 'What if I have issues accessing my character information on the Medor Roster website?',
-        text: 'If you have any issues accessing your character information on the Medor Roster website, please contact your admin for assistance.'
+        texts: 'If you have any issues accessing your character information on the Medor Roster website, please contact your admin for assistance.'
     },
     {
         heading: 'Is the Medor Roster website affiliated with EVE Online?',
-        text: 'No, the Medor Roster website is not affiliated with EVE Online or its parent company CCP Games.'
+        texts: 'No, the Medor Roster website is not affiliated with EVE Online or its parent company CCP Games.'
     },
 ]
 
 const FAQPage: React.FC = () => {
-    return <main className="text-slate-200 px-4 sm:px-8 md:px-24 lg:px-32 flex flex-col items-center">
+    return <main>
         <Header/>
-        <h1 className="text18-30 font-bold">Frequently Asked Questions (FAQ)</h1>
+        <h1 className="text30-48 font-bold mt-5 sm:mt-10">Frequently Asked Questions (FAQ)</h1>
         <Paragraphs
             className="gap-y-5 mt-5 sm:mt-10 max-w-4xl"
             itemClassNames={{
-                headingClassName: 'text14-20 font-semibold',
+                className: 'gap-y-2.5',
+                headingClassName: 'text18-20 font-semibold',
                 textClassName: 'text16-18'
             }}
             data={data}
---
+++ frontend/src/pages/privacyPolicy/index.tsx
@@ -0,0 +1,113 @@
+import Header from "../../layout/header";
+import Paragraphs from "../../ui/paragraphs";
+import { ParagraphType } from "../../ui/paragraphs/paragraph";
+import moment from 'moment';
+
+const data: ParagraphType[] = [
+    {
+        heading: 'Personal Information We Collect',
+        texts: [
+            'When you visit the Medor Roster website, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Medor Roster website, we collect information about the individual web pages that you view, what websites or search terms referred you to the Medor Roster website, and information about how you interact with the Medor Roster website.',
+            'When you register for the Medor Roster website, you will be asked to submit your email address and password. An invatation link will be automaticly added from the URL. We use this information to allow you access to the Medor Roster website.'
+        ]
+    },
+    {
+        heading: 'We collect information using the following technologies:',
+        texts: [
+            [
+                {
+                    text: '"Cookies"',
+                },
+                'are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit',
+                {
+                    text: 'http://www.allaboutcookies.org',
+                    effect: 'link'
+                },
+                '.'
+            ],
+            [
+                {
+                    text: '"Log files"',
+                },
+                'track actions occurring on the Medor Roster website, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.'
+            ],
+            [
+                {
+                    text: '"Web beacons"',
+                    shouldAddSpace: false,
+                },
+                ',',
+                {
+                    text: '"tags"',
+                },
+                'and',
+                {
+                    text: '"pixels"',
+                },
+                'are electronic files used to record information about how you browse the Website.'
+            ],
+        ]
+    },
+    {
+        heading: 'Sharing Your Personal Information',
+        texts: 'We do not share your Personal Information with third parties.We may share your Personal Information to comply with applicable laws and regulations.'
+    },
+    {
+        heading: 'Your Rights',
+        texts: 'You have the right to access personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below.'
+    },
+    {
+        heading: 'Data Retention',
+        texts: 'We will retain your Personal Information only for as long as necessary to provide you with access to the Medor Roster website and as described in this Privacy Policy.'
+    },
+    {
+        heading: 'Changes',
+        texts: 'We may update this Privacy Policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.'
+    },
+    {
+        heading: 'Contact Us',
+        texts: [
+            [
+                'For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at',
+                {
+                    text: 'privacy@medorRoster.comor',
+                    effect: 'email'
+                },
+                'using the details provided below:',
+            ],
+            'Medor Roster, USA, city.',
+        ]
+    },
+];
+
+const lastEditTime = '2023-07-24T16:39:55.000Z'; //from the database
+
+const PrivacyPolicyPage: React.FC = () => {
+    let editTime = moment(lastEditTime).format('MMMM Do YYYY, hh:mm:ss a');
+
+    return <main>
+        <Header />
+        <section className="max-w-4xl">
+            <div className="flex flex-col items-center">
+                <h1 className="text30-48 font-bold mt-5 sm:mt-10">Privacy policy</h1>
+                <p className="text16-18 text-center mt-2.5 sm:mt-5">This Privacy Policy describes how your personal information is collected, used, and shared when you visit the Medor Roster website</p>
+            </div>
+            <Paragraphs
+                className="gap-y-5 mt-5 sm:gap-y-8 sm:mt-10"
+                itemClassNames={{
+                    className: 'gap-y-3',
+                    headingClassName: 'text18-20 font-semibold',
+                    textsContainerClassName: 'gap-y-2',
+                    textClassName: 'text16-18'
+                }}
+                data={data}
+            />
+            <h6 className="mr-auto mt-5">
+                <span className="font-bold">Last edit time: </span>
+                <time>{editTime}</time>
+            </h6>
+        </section>
+    </main>
+};
+
+export default PrivacyPolicyPage;
