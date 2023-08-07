6f8c61ba7fab58e9c35e8be5365d2896821b1fb6
podpison <au25557@yandex.ru>
2023-08-07 23:51:27 +0500

    added useMemo for the createCharacterModal.tsx file

--- frontend/src/pages/dashboard/createCharacterModal.tsx
+++ frontend/src/pages/dashboard/createCharacterModal.tsx
@@ -22,8 +22,6 @@ const validationSchema = Yup.object({
 });
 
 const CreateCharacterModal: React.FC<Props> = ({ isOpen, onClose, isThereMain }) => {
-  // const inputs: FormInputType[] = 
-
   const inputs: FormInputType[] = useMemo(() => {
     return [
       "Name",
