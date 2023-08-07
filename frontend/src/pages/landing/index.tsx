8326e9d0084b939b3bfef32d116c27ab8d14e15c
wiktor-falek <falekwiktor@gmail.com>
2023-08-07 20:04:42 +0200

    adjust button gap

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -7,7 +7,7 @@ const LandingPage: React.FC = () => {
         <main
             style={{ backgroundImage: `url(${background})` }}
             className="flex flex-col items-center bg-center bg-no-repeat bg-cover h-screen">
-            <div className="flex flex-col justify-between items-center mt-20 gap-80">
+            <div className="flex flex-col justify-between items-center mt-20 gap-32">
                 <div className="flex flex-col text-center gap-6">
                     <h1 className="text-7xl text-slate-100">Medorium Roster</h1>
                     <h2 className="text-3xl">Best way to blah blah blah...</h2>
