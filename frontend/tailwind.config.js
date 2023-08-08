d945a2ed65a03e3e6d15581dc1ecc553769e305e
wiktor-falek <falekwiktor@gmail.com>
2023-08-08 14:10:00 +0200

    try adding font (doesn't work ._.)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
             },
             container: {
                 center: true
+            },
+            fontFamily: {
+                'shentox': ['Shentox']
             }
         },
     },
