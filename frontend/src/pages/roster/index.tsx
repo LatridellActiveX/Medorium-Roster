5fa1d5b96d923f1f5312249aec6c67a314703268
wiktor-falek <falekwiktor@gmail.com>
2023-08-31 19:43:56 +0200

    sort and filter select, search input, styling

--- frontend/src/pages/roster/index.tsx
+++ frontend/src/pages/roster/index.tsx
@@ -13,7 +13,32 @@ const RosterPage: React.FC = () => {
   return (
     <PageInitialization pathIfUnauth='/login'>
       <main className="py-5">
-        <section className="w-[600px] mx-auto my-5">
+        <section className="w-[600px] mx-auto my-5 space-y-2">
+          <div className="flex justify-between" id="options">
+            <div className="flex gap-2">
+              {/* sort */}
+              <div className="flex flex-col">
+                <label htmlFor="sort">Sort By</label >
+                <select id="sort">
+                  <option value="date-added">Date Added</option>
+                </select>
+              </div>
+
+              {/* filter */}
+              <div className="flex flex-col">
+                <label htmlFor="filter">Filter</label >
+                <select id="filter">
+                  <option value="none" defaultChecked>None</option>
+                </select>
+              </div>
+            </div>
+
+            {/* search */}
+            <div className="flex items-end">
+              <input className="select" type="text" placeholder="Search..." />
+            </div>
+          </div>
+
           <Characters
             data={data || []}
             isLoading={isLoading}
