9d43302de97513da4b00f4ec03dd7fc27b94f101
podpison <au25557@yandex.ru>
2023-09-02 21:18:31 +0500

    updated registration tests

--- frontend/src/__tests__/registrationForm.test.tsx
+++ frontend/src/__tests__/registrationForm.test.tsx
@@ -13,59 +13,56 @@ describe("Registration form", () => {
     );
   });
 
-  it('I will it later', () => {
-    
-  })
-  // it("types too long username", async () => {
-  //   const tooLongUsername = "I".repeat(50);
-  //   const password = "1231";
+  it("types too long username", async () => {
+    const tooLongUsername = "I".repeat(50);
+    const password = "1231";
 
-  //   let usernameInput = screen.getByRole("textbox", { name: /Username/i });
-  //   let passwordInput = screen.getByLabelText("password");
-  //   let submitBtn = screen.getByRole("button", {
-  //     name: "Submit your registration credentials",
-  //   });
+    let usernameInput = screen.getByRole("textbox", { name: /Username/i });
+    let passwordInput = screen.getByLabelText("password");
+    let submitBtn = screen.getByRole("button", {
+      name: "Submit your credentials",
+    });
 
-  //   await user.type(usernameInput, tooLongUsername);
-  //   await user.type(passwordInput, password);
+    await user.type(usernameInput, tooLongUsername);
+    await user.type(passwordInput, password);
 
-  //   expect(submitBtn.getAttribute("disabled")).toBe("");
-  // });
-  // it("does not provide any credentials", () => {
-  //   let submitBtn = screen.getByRole("button", {
-  //     name: "Submit your registration credentials",
-  //   });
+    expect(submitBtn.getAttribute("disabled")).toBe("");
+  });
+  it("does not provide any credentials", () => {
+    let submitBtn = screen.getByRole("button", {
+      name: "Submit your credentials",
+    });
 
-  //   expect(submitBtn.getAttribute("disabled")).toBe(null);
-  // });
-  // it("types too short username and password", async () => {
-  //   const tooShortUsername = "aa";
-  //   const tooShortPasssword = "11";
+    expect(submitBtn.getAttribute("disabled")).toBe(null);
+  });
+  it("types too short username and password", async () => {
+    const tooShortUsername = "aa";
+    const tooShortPasssword = "11";
 
-  //   let usernameInput = screen.getByRole("textbox", { name: /Username/i });
-  //   let passwordInput = screen.getByLabelText("password");
-  //   let submitBtn = screen.getByRole("button", {
-  //     name: "Submit your registration credentials",
-  //   });
+    let usernameInput = screen.getByRole("textbox", { name: /Username/i });
+    let passwordInput = screen.getByLabelText("password");
+    let submitBtn = screen.getByRole("button", {
+      name: "Submit your credentials",
+    });
 
-  //   await user.type(usernameInput, tooShortUsername);
-  //   await user.type(passwordInput, tooShortPasssword);
+    await user.type(usernameInput, tooShortUsername);
+    await user.type(passwordInput, tooShortPasssword);
 
-  //   expect(submitBtn.getAttribute("disabled")).toBe("");
-  // });
-  // it("types correct credentials", async () => {
-  //   const username = "username";
-  //   const password = "password";
+    expect(submitBtn.getAttribute("disabled")).toBe("");
+  });
+  it("types correct credentials", async () => {
+    const username = "username";
+    const password = "password";
 
-  //   let usernameInput = screen.getByRole("textbox", { name: /Username/i });
-  //   let passwordInput = screen.getByLabelText("password");
-  //   let submitBtn = screen.getByRole("button", {
-  //     name: "Submit your registration credentials",
-  //   });
+    let usernameInput = screen.getByRole("textbox", { name: /Username/i });
+    let passwordInput = screen.getByLabelText("password");
+    let submitBtn = screen.getByRole("button", {
+      name: "Submit your credentials",
+    });
 
-  //   await user.type(usernameInput, username);
-  //   await user.type(passwordInput, password);
+    await user.type(usernameInput, username);
+    await user.type(passwordInput, password);
 
-  //   expect(submitBtn.getAttribute("disabled")).toBe(null);
-  // });
+    expect(submitBtn.getAttribute("disabled")).toBe(null);
+  });
 });
